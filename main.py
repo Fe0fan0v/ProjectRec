e679b08b95d001a7dbf5b0935bd13f8e33ac2989
vadim <feofanov@yandexlyceum.ru>
2020-10-18 19:10:33 +0500

    New

--- main.py
+++ main.py
@@ -1,34 +1,105 @@
-# Import all necessary libraries.
-import os
 import numpy as np
-import sys
-import glob
-import matplotlib.image as mpimg
-import matplotlib.pyplot as plt
+import cv2
+from PIL import Image
+import pytesseract as tess
+def clean2_plate(plate):
+    gray_img = cv2.cvtColor(plate, cv2.COLOR_BGR2GRAY)
 
-# NomeroffNet path
-NOMEROFF_NET_DIR = os.path.abspath('/home/vadim/PycharmProjects/ProjectRec/nomeroff-net/')
-sys.path.append(NOMEROFF_NET_DIR)
+    _, thresh = cv2.threshold(gray_img, 110, 255, cv2.THRESH_BINARY)
+    if cv2.waitKey(0) & 0xff == ord('q'):
+        pass
+    num_contours,hierarchy = cv2.findContours(thresh.copy(),cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
-# Import license plate recognition tools.
-from NomeroffNet import Detector
-from NomeroffNet import filters
+    if num_contours:
+        contour_area = [cv2.contourArea(c) for c in num_contours]
+        max_cntr_index = np.argmax(contour_area)
 
-# load model
-nnet = Detector()
-nnet.loadModel(NOMEROFF_NET_DIR)
+        max_cnt = num_contours[max_cntr_index]
+        max_cntArea = contour_area[max_cntr_index]
+        x,y,w,h = cv2.boundingRect(max_cnt)
 
-# Walking through the ./examples/images/ directory and checking each of the images for license plates.
-rootDir = '/images/'
+        if not ratioCheck(max_cntArea,w,h):
+            return plate,None
 
-imgs = [mpimg.imread(img_path) for img_path in glob.glob(rootDir)]
+        final_img = thresh[y:y+h, x:x+w]
+        return final_img,[x,y,w,h]
 
-cv_imgs_masks = nnet.detect_mask(imgs)
+    else:
+        return plate,None
 
-for img, cv_img_masks in zip(imgs, cv_imgs_masks):
-    # Generate splashs.
-    splashs = filters.color_splash(img, cv_img_masks)
-    for splash in splashs:
-        plt.imshow(splash)
-        plt.axis("off")
-        plt.show()
+def ratioCheck(area, width, height):
+    ratio = float(width) / float(height)
+    if ratio < 1:
+        ratio = 1 / ratio
+    if (area < 1063.62 or area > 73862.5) or (ratio < 3 or ratio > 6):
+        return False
+    return True
+
+def isMaxWhite(plate):
+    avg = np.mean(plate)
+    if avg >= 115:
+        return True
+    else:
+        return False
+
+def ratio_and_rotation(rect):
+    (x, y), (width, height), rect_angle = rect
+
+    if width > height:
+        angle = -rect_angle
+    else:
+        angle = 90 + rect_angle
+
+    if angle>15:
+        return False
+
+    if height == 0 or width == 0:
+        return False
+
+    area = height*width
+    if not ratioCheck(area,width,height):
+        return False
+    else:
+        return True
+
+img = cv2.imread("img.jpg")
+print("Number  input image...",)
+cv2.imshow("input", img)
+
+if cv2.waitKey(0) & 0xff == ord('q'):
+    pass
+img2 = cv2.GaussianBlur(img, (3,3), 0)
+img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+img2 = cv2.Sobel(img2,cv2.CV_8U,1,0,ksize=3)	
+_,img2 = cv2.threshold(img2,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+element = cv2.getStructuringElement(shape=cv2.MORPH_RECT, ksize=(17, 3))
+morph_img_threshold = img2.copy()
+cv2.morphologyEx(src=img2, op=cv2.MORPH_CLOSE, kernel=element, dst=morph_img_threshold)
+num_contours, hierarchy= cv2.findContours(morph_img_threshold,mode=cv2.RETR_EXTERNAL,method=cv2.CHAIN_APPROX_NONE)
+cv2.drawContours(img2, num_contours, -1, (0,255,0), 1)
+
+
+for i,cnt in enumerate(num_contours):
+
+    min_rect = cv2.minAreaRect(cnt)
+
+    if ratio_and_rotation(min_rect):
+
+        x,y,w,h = cv2.boundingRect(cnt)
+        plate_img = img[y:y+h,x:x+w]
+        print("Number  identified number plate...")
+        cv2.imshow("num plate image", plate_img)
+        if cv2.waitKey(0) & 0xff == ord('q'):
+            pass
+
+        if(isMaxWhite(plate_img)):
+            clean_plate, rect = clean2_plate(plate_img)
+            if rect:
+                fg=0
+                x1,y1,w1,h1 = rect
+                x,y,w,h = x+x1,y+y1,w1,h1
+                plate_im = Image.fromarray(clean_plate)
+                text = tess.image_to_string(plate_im, lang='eng')
+                print("Number  Detected Plate Text : ", text)
